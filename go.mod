fb46702ed099d27a6bada22db8897a091ec0cea0
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-09-21 15:35:43 +0800

    jwt——token验证

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
---
+++ utils/tokens/jwt.go
@@ -0,0 +1,45 @@
+package tokens
+
+import (
+	"GuTikTok/config"
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+
+	"time"
+)
+
+type MyClaims struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+// GetToken 生成token
+func GetToken(id int64, username string) (string, error) {
+	expireTime := time.Now().Add(time.Hour * 24 * 90) // 三个月过期
+	SetClaims := MyClaims{
+		id,
+		username,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expireTime),
+			Issuer:    "ByteHunters",
+		},
+	}
+	reqClaim := jwt.NewWithClaims(jwt.SigningMethodHS256, SetClaims)
+	return reqClaim.SignedString([]byte(config.Conf.JwtSecret))
+}
+
+// CheckToken 验证token
+func CheckToken(token string) (*MyClaims, error) {
+	key, err := jwt.ParseWithClaims(token, &MyClaims{}, func(*jwt.Token) (any, error) {
+		return []byte(config.Conf.JwtSecret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := key.Claims.(*MyClaims); ok && key.Valid {
+		return claims, nil
+	} else {
+		return nil, errors.New("你的token已过期")
+	}
+}
