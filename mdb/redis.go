14c0fceb2d305f3a3926ccff2618aeddbe40bf92
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-09-25 11:42:32 +0800

    使用UniversalClient连接数据库

--- mdb/redis.go
+++ mdb/redis.go
@@ -9,16 +9,21 @@ import (
 	"time"
 )
 
-var rdb *redis.Client //操作redis入口
+var rdb redis.UniversalClient //操作redis入口
+const redisName = ""
 
 // InitRdb 初始化 Redis
 func InitRdb() {
 	log.Info("开始初始化 Redis 服务!")
 	rconf := config.Conf.Redis
-	rdb = redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%d", rconf.Host, rconf.Port),
-		Password: rconf.Password,
-		DB:       rconf.Db,
+	redis_addr := fmt.Sprintf("%s:%d", rconf.Host, rconf.Port)
+	rdb = redis.NewUniversalClient(&redis.UniversalOptions{
+		Addrs: []string{
+			redis_addr,
+		},
+		Password:   rconf.Password,
+		DB:         rconf.Db,
+		MasterName: redisName,
 	})
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
