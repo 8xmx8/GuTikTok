5072f5441ebed15ae494f6fe34e6b42d5d4d062f
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-09-27 19:40:54 +0800

    Consul API将服务注册到Consul

---
+++ utils/consul/register.go
@@ -0,0 +1,54 @@
+package consul
+
+import (
+	"GuTikTok/config"
+	"GuTikTok/utils/logging"
+	"fmt"
+	"github.com/google/uuid"
+	capi "github.com/hashicorp/consul/api"
+	log "github.com/sirupsen/logrus"
+	"strconv"
+)
+
+var consulClient *capi.Client
+
+func init() {
+	cfg := capi.DefaultConfig()
+	cfg.Address = config.Conf.Server.Address
+	if c, err := capi.NewClient(cfg); err == nil {
+		consulClient = c
+		return
+	} else {
+		logging.Logger.Panicf("Connect Consul happens error: %v", err)
+	}
+}
+
+func RegisterConsul(name string, port string) error {
+	parsedPort, err := strconv.Atoi(port[1:]) // port start with ':' which like ':37001'
+	logging.Logger.WithFields(log.Fields{
+		"name": name,
+		"port": parsedPort,
+	}).Infof("Services Register Consul")
+	name = "CONSUL_ANONYMITY_NAME" + name
+
+	if err != nil {
+		return err
+	}
+	reg := &capi.AgentServiceRegistration{
+		ID:      fmt.Sprintf("%s-%s", name, uuid.New().String()[:5]),
+		Name:    name,
+		Address: config.Conf.Server.Address,
+		Port:    parsedPort,
+		Check: &capi.AgentServiceCheck{
+			Interval:                       "5s",
+			Timeout:                        "5s",
+			GRPC:                           fmt.Sprintf("%s:%d", config.Conf.Server.Address, parsedPort),
+			GRPCUseTLS:                     false,
+			DeregisterCriticalServiceAfter: "30s",
+		},
+	}
+	if err := consulClient.Agent().ServiceRegister(reg); err != nil {
+		return err
+	}
+	return nil
+}
