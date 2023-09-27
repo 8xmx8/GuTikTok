4cd0092388ab31e78de802b3a6f49b4b8409d46f
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-09-27 19:38:34 +0800

    web服务端口和grpc服务端口

---
+++ config/service.go
@@ -0,0 +1,42 @@
+package config
+
+const WebServiceName = "GuTikTok-GateWay"
+const WebServiceAddr = ":37000"
+
+const AuthRpcServerName = "GuTikTok-AuthService"
+const AuthRpcServerPort = ":37001"
+
+const UserRpcServerName = "GuTikTok-UserService"
+const UserRpcServerPort = ":37002"
+
+const CommentRpcServerName = "GuTikTok-CommentService"
+const CommentRpcServerPort = ":37003"
+
+const FeedRpcServerName = "GuTikTok-FeedService"
+const FeedRpcServerPort = ":37004"
+
+const PublishRpcServerName = "GuTikTok-PublisherService"
+const PublishRpcServerPort = ":37005"
+
+const FavoriteRpcServerName = "GuTikTok-FavoriteService"
+const FavoriteRpcServerPort = ":37006"
+
+const MessageRpcServerName = "GuTikTok-MessageService"
+const MessageRpcServerPort = ":37007"
+
+const RelationRpcServerName = "GuTikTok-RelationService"
+const RelationRpcServerPort = ":37008"
+
+const RecommendRpcServiceName = "GuTikTok-Recommend"
+const RecommendRpcServicePort = ":37009"
+
+const Metrics = ":37099"
+const VideoProcessorRpcServiceName = "GuTikTok-VideoProcessorService"
+
+const VideoPicker = "GuTikTok-VideoPicker"
+const Event = "GuTikTok-Recommend"
+const MsgConsumer = "GuTikTok-MgsConsumer"
+
+const BloomRedisChannel = "GuTikTok-Bloom"
+
+const MaxVideoSize = 200 * 1024 * 1024
