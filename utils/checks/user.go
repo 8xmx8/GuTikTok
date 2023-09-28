f6b54fefe5be24258ce9ec38b32b6475c17eccd4
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-09-28 12:06:09 +0800

    密码校验: 限制密码长度，含数字，字母，特殊符号其两种即可

--- utils/checks/user.go
+++ utils/checks/user.go
@@ -3,9 +3,10 @@ package checks
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
-// ValidateInput 输入内容验证
+// ValidateInput 输入内容验证   2023-9-28 废除此方案
 func ValidateInput(minLen, maxLen int, s ...string) string {
 	for i := range s {
 		n := len(s[i])
@@ -26,4 +27,57 @@ func ValidateInput(minLen, maxLen int, s ...string) string {
 func isValidString(s string) bool {
 	ok, _ := regexp.MatchString("^[a-zA-Z0-9\\.!@$%#]+$", s)
 	return ok
+}
+
+// ValidatePassword 密码校验
+func ValidatePassword(password string, min, max int) bool {
+	// 验证密码长度是否大于8
+	if len(password) < min && len(password) > max {
+		return false
+	}
+
+	// 验证密码是否包含至少两种类型的字符（英文字母、数字、特殊字符）
+	typesCount := 0
+
+	// 包含英文字母
+	if containsLetter(password) {
+		typesCount++
+	}
+
+	// 包含数字
+	if containsDigit(password) {
+		typesCount++
+	}
+
+	// 包含特殊字符
+	if containsSpecialChar(password) {
+		typesCount++
+	}
+
+	return typesCount >= 2
+}
+
+func containsLetter(password string) bool {
+	// 使用正则表达式验证密码是否包含英文字母
+	pattern := `[a-zA-Z]`
+	regex := regexp.MustCompile(pattern)
+	return regex.MatchString(password)
+}
+
+func containsDigit(password string) bool {
+	// 使用正则表达式验证密码是否包含数字
+	pattern := `[0-9]`
+	regex := regexp.MustCompile(pattern)
+	return regex.MatchString(password)
+}
+
+func containsSpecialChar(password string) bool {
+	// 使用字符串包含函数验证密码是否包含特殊字符
+	specialChars := []string{"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "<", ">", "?", "~"}
+	for _, char := range specialChars {
+		if strings.Contains(password, char) {
+			return true
+		}
+	}
+	return false
 }
