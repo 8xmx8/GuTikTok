a9bb9040ef447ec4308027a5286392ebca99b415
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-10-07 11:23:31 +0800

    令牌桶限流

---
+++ src/web/middleware/limiter.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/juju/ratelimit"
+	"net/http"
+	"time"
+)
+
+func RateLimiterMiddleWare(fillInterval time.Duration, cap, quantum int64) gin.HandlerFunc {
+	bucket := ratelimit.NewBucketWithQuantum(fillInterval, cap, quantum)
+	return func(c *gin.Context) {
+		if bucket.TakeAvailable(1) < 1 {
+			c.String(http.StatusForbidden, "rate limit...")
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
