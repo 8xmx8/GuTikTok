581743b2d8beee4f75f772457c86fa733555ca0c
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-11-02 20:07:55 +0800

    用户关系服务，准备引入RabbitMQ

--- src/services/relation/handler.go
+++ src/services/relation/handler.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"GuTikTok/config"
+	"GuTikTok/src/rpc/relation"
+	"GuTikTok/src/rpc/user"
+	"fmt"
+)
 
+var userClient user.UserServiceClient
+
+var actionRelationLimitKeyPrefix = config.Conf.Redis.RedisPrefix + "relation_freq_limit"
+
+const actionRelationMaxQPS = 3
+
+type RelationServiceImpl struct {
+	relation.RelationServiceServer
+}
+
+func actionRelationLimitKey(userId uint32) string {
+	return fmt.Sprintf("%s-%d", actionRelationLimitKeyPrefix, userId)
+}
+
+func exitOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
